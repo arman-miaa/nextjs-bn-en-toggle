89430c87384a080529cd3e370e10bb68ab598bb7
Arman Mia <mdarman365506@gmail.com>
2025-10-17 17:04:28 +0600

    Disable browser auto-translate and setup basic language toggle with next-intl

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -33,6 +33,7 @@ export default async function RootLayout({ children, params }) {
 
   return (
     <html lang={locale} translate="no">
+      <meta name="google" content="notranslate" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
